ae042eeea6d9bc8e2c02ca036dd042723a10f17c
Roque Santos <roque-100@hotmail.com>
2025-10-20 21:06:52 -0300

    day 003: allocating a backbuffer

--- src/win32_main.cpp
+++ src/win32_main.cpp
@@ -1,8 +1,68 @@
 #include <windows.h>
-#include <wingdi.h>
-#include <winuser.h>
 
-LRESULT CALLBACK main_window_callback(
+// just a nice way of differentiating the uses of "static" keyword,
+// which can mean different thing in different contexts
+#define internal      static // a function that is internal to a file, not usable outside a source file (private-like)
+#define local_persist static // a variable that is persistent inside a scope (keeps its value when program go out of the scope and return to it later)
+#define global_var    static // a global variable... thats just it
+
+// TODO: global for now, struct in the future, when? dunno
+global_var bool running;
+
+global_var BITMAPINFO bitmap_info;
+global_var void* bitmap_memory;
+global_var HBITMAP bitmap_handle;
+global_var HDC bitmap_device_ctx;
+
+/*
+	DIB = device independant bitmap
+	> "Things that you can write into as bitmaps that it (Windows) can then display using GDI" - Muratori, C
+	GDI = windows graphics rendering thing
+*/
+internal void win32_resize_dbi_section(int w, int h) {
+
+	if (bitmap_handle) {
+		DeleteObject(bitmap_handle);
+	}
+
+	if (!bitmap_device_ctx) {
+		bitmap_device_ctx = CreateCompatibleDC(0);
+	}
+
+	bitmap_info = {
+		.bmiHeader = {
+			.biSize = sizeof(bitmap_info.bmiHeader),
+			.biWidth = w,
+			.biHeight = h,
+			.biPlanes = 1,
+			.biBitCount = 32,
+			.biCompression = BI_RGB,
+		},
+	};
+
+
+	bitmap_handle = CreateDIBSection(
+		bitmap_device_ctx,
+		&bitmap_info,
+		DIB_RGB_COLORS,
+		&bitmap_memory,
+		0, 0
+	);
+}
+
+internal void win32_update_window(HDC device_ctx, int x, int y, int w, int h) {
+	StretchDIBits(
+		device_ctx,
+		x, y, w, h, // section of the window to fill - front buffer
+		x, y, w, h, // source of the render - back buffer
+		bitmap_memory,
+		&bitmap_info,
+		DIB_RGB_COLORS,
+		SRCCOPY
+	);
+}
+
+LRESULT CALLBACK win32_main_window_callback(
   HWND window,
   UINT message,
   WPARAM w_param,
@@ -12,24 +72,33 @@ LRESULT CALLBACK main_window_callback(
 
 	switch (message) {
 
-		case WM_SIZE: {
+		case WM_SIZE: { // INFO: Window resize
 			OutputDebugStringA("WM_SIZE\n");
+
+			RECT client_rect;
+			GetClientRect(window, &client_rect);
+			int w = client_rect.right - client_rect.left;
+			int h = client_rect.bottom - client_rect.top;
+			win32_resize_dbi_section(w, h);
 		} break;
 
-		case WM_DESTROY: {
+		case WM_DESTROY: { // INFO: window gets destroyed
+			               // TODO: potential error - recreate window?
 			OutputDebugStringA("WM_DESTROY\n");
+			running = false;
 		} break;
 	
-		case WM_CLOSE: {
+		case WM_CLOSE: { // INFO: window wants to close
+			             // TODO: message user?
 			OutputDebugStringA("WM_CLOSE\n");
-			PostQuitMessage(1);
+			running = false;
 		} break;
 	
-		case WM_ACTIVATEAPP: {
+		case WM_ACTIVATEAPP: { // INFO: window get focused/active
 			OutputDebugStringA("WM_ACTIVATEAPP\n");
 		} break;
 	
-		case WM_PAINT: {
+		case WM_PAINT: { // INFO: painting on the window
 			PAINTSTRUCT paint;
 			HDC hdc = BeginPaint(window, &paint);
 
@@ -38,22 +107,12 @@ LRESULT CALLBACK main_window_callback(
 			int x = paint.rcPaint.left;
 			int y = paint.rcPaint.top;
 
-			PatBlt(hdc, x, y, w, h, WHITENESS);
-
-			SetPixel(hdc, 100, 100, RGB(255, 0, 0));
-			SetPixel(hdc, 101, 100, RGB(255, 0, 0));
-			SetPixel(hdc, 102, 100, RGB(255, 0, 0));
-			SetPixel(hdc, 103, 100, RGB(255, 0, 0));
-			SetPixel(hdc, 104, 100, RGB(255, 0, 0));
-			SetPixel(hdc, 105, 100, RGB(255, 0, 0));
-			SetPixel(hdc, 106, 100, RGB(255, 0, 0));
-			SetPixel(hdc, 107, 100, RGB(255, 0, 0));
-			SetPixel(hdc, 108, 100, RGB(255, 0, 0));
+			win32_update_window(hdc, x, y, w, h);
 
 			EndPaint(window, &paint);
 		} break;
 
-		default: {
+		default: { // INFO: unhandled stuff
 			// OutputDebugStringA("default\n");
 			result = DefWindowProc(window, message, w_param, l_param);
 		} break;
@@ -65,7 +124,7 @@ LRESULT CALLBACK main_window_callback(
 int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int show_cmd) {
 	WNDCLASS window_class = {
 		.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
-		.lpfnWndProc = main_window_callback,
+		.lpfnWndProc = win32_main_window_callback,
 		.hInstance = instance,
 		// .hIcon = 
 		.lpszClassName = "handmade_hero_window_class",
@@ -89,8 +148,9 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line
 
 		if (window_handle) {
 			MSG message;
+			running = 1;
 
-			for (;;) {
+			while (running) {
 				BOOL msg_result = GetMessage(&message, 0, 0, 0);
 
 				if (msg_result <= 0) break;
