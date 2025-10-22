04a3777c279a1e6680660467a174ba8ce93d9c48
Roque Santos <roque-100@hotmail.com>
2025-10-22 01:16:33 -0300

    day 005: clean up and refactor

--- src/win32_main.cpp
+++ src/win32_main.cpp
@@ -1,7 +1,5 @@
 #include <cstdint>
 #include <windows.h>
-#include <wingdi.h>
-#include <winuser.h>
 
 // just a nice way of differentiating the uses of "static" keyword,
 // which can mean different thing in different contexts
@@ -19,27 +17,47 @@ typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
 
-// TODO: global for now, struct in the future, when? dunno
-global_var bool running;
-global_var BITMAPINFO bitmap_info;
-global_var void* bitmap_memory;
-global_var int bitmap_width;
-global_var int bitmap_height;
-global_var const int PIXEL_SIZE_IN_BYTES = 4;
+struct win32_bmp_buffer {
+	BITMAPINFO info;
+	void* memory;
+	int width;
+	int height;
+	int pitch;
+	int pixel_size_in_bytes;
+};
 
-internal void render_cool_gradient(int x_offset, int y_offset) {
-	int pitch = bitmap_width * PIXEL_SIZE_IN_BYTES;
-	uint8* row = (uint8*)bitmap_memory;
-	for (int y = 0; y < bitmap_height; y++) {
+struct win32_window_dimension {
+	int width; int height;
+};
+
+global_var bool running;
+global_var win32_bmp_buffer backbuf;
+
+win32_window_dimension win32_get_window_dimension(HWND window) {
+	RECT client_rect;
+	GetClientRect(window, &client_rect);
+
+	return {
+		.width = client_rect.right - client_rect.left,
+		.height = client_rect.bottom - client_rect.top,
+	};
+}
+
+internal void render_cool_gradient(win32_bmp_buffer buf, int x_offset, int y_offset) {
+	uint8* row = (uint8*)buf.memory;
+	for (int y = 0; y < buf.height; y++) {
 		uint32* pixel = (uint32*)row;
-		for (int x = 0; x < bitmap_width; x++) {
+		for (int x = 0; x < buf.width; x++) {
 			uint8 b = x + x_offset;
 			uint8 g = y + y_offset;
 
+			// 1. shift "g" left by 8 bits: 00 00 gg > 00 gg 00 
+			// 2. bitwise or "g" and "b": 00 gg 00 | 00 00 bb = 00 gg bb
+			// 3. post increment pixel pointer to next pixel
 			*pixel++ = (g << 8) | b;
 		}
 
-		row += pitch;
+		row += buf.pitch;
 	}
 }
 
@@ -48,44 +66,39 @@ internal void render_cool_gradient(int x_offset, int y_offset) {
 	> "Things that you can write into as bitmaps that it (Windows) can then display using GDI" - Muratori, C
 	GDI = windows graphics rendering thing
 */
-internal void win32_resize_dbi_section(int w, int h) {
-
-	if (bitmap_memory) {
-		VirtualFree(bitmap_memory, 0, MEM_RELEASE);
+internal void win32_resize_dbi_section(win32_bmp_buffer* buf, int w, int h) {
+	if (buf->memory) {
+		VirtualFree(buf->memory, 0, MEM_RELEASE);
 	}
 
-	bitmap_width = w;
-	bitmap_height = h;
-
-	bitmap_info = {
+	buf->width = w;
+	buf->height = h;
+	buf->pixel_size_in_bytes = 4; // INFO: 4 bytes in order to align with biBitCount of 32 below
+	buf->info = {
 		.bmiHeader = {
-			.biSize = sizeof(bitmap_info.bmiHeader),
-			.biWidth = bitmap_width,
-			.biHeight = -bitmap_height,
+			.biSize = sizeof(buf->info.bmiHeader),
+			.biWidth = buf->width,
+			.biHeight = -buf->height,
 			.biPlanes = 1,
 			.biBitCount = 32,
 			.biCompression = BI_RGB,
 		},
 	};
 
-	// INFO: 4 bytes in order to align with biBitCount of 32 above
 	// 8b R + 8b G + 8b B + 8b ALIGNMENT PADDING = 32bits (4 bytes)
-	int bitmap_memory_size = bitmap_width * bitmap_height * PIXEL_SIZE_IN_BYTES;
-	bitmap_memory = VirtualAlloc(0, bitmap_memory_size, MEM_COMMIT, PAGE_READWRITE);
+	int bitmap_memory_size = buf->width * buf->height * buf->pixel_size_in_bytes;
+	buf->memory = VirtualAlloc(0, bitmap_memory_size, MEM_COMMIT, PAGE_READWRITE);
+	buf->pitch = buf->width * buf->pixel_size_in_bytes;
 }
 
-internal void win32_update_window(HDC device_ctx, RECT *window_rect, int x, int y, int w, int h) {
-	int window_width = window_rect->right - window_rect->left;
-	int window_height = window_rect->bottom - window_rect->top;
-
+internal void win32_display_buffer(HDC device_ctx, int width, int height, win32_bmp_buffer buf) {
+	// TODO: aspect my ratio
 	StretchDIBits(
 		device_ctx,
-		// x, y, w, h, // section of the window to fill - front buffer
-		// x, y, w, h, // source of the render - back buffer
-		0, 0, bitmap_width, bitmap_height,
-		0, 0, window_width, window_height,
-		bitmap_memory,
-		&bitmap_info,
+		0, 0, width, height, // source of the render - back buffer
+		0, 0, buf.width, buf.height, // section of the window to fill - front buffer
+		buf.memory,
+		&buf.info,
 		DIB_RGB_COLORS,
 		SRCCOPY
 	);
@@ -103,12 +116,6 @@ LRESULT CALLBACK win32_main_window_callback(
 
 		case WM_SIZE: { // INFO: Window resize
 			OutputDebugStringA("WM_SIZE\n");
-
-			RECT client_rect;
-			GetClientRect(window, &client_rect);
-			int w = client_rect.right - client_rect.left;
-			int h = client_rect.bottom - client_rect.top;
-			win32_resize_dbi_section(w, h);
 		} break;
 
 		case WM_DESTROY: { // INFO: window gets destroyed
@@ -131,14 +138,8 @@ LRESULT CALLBACK win32_main_window_callback(
 			PAINTSTRUCT paint;
 			HDC hdc = BeginPaint(window, &paint);
 
-			int w = paint.rcPaint.right - paint.rcPaint.left;
-			int h = paint.rcPaint.bottom - paint.rcPaint.top;
-			int x = paint.rcPaint.left;
-			int y = paint.rcPaint.top;
-
-			RECT client_rect;
-			GetClientRect(window, &client_rect);
-			win32_update_window(hdc, &client_rect, x, y, w, h);
+			win32_window_dimension dim = win32_get_window_dimension(window);
+			win32_display_buffer(hdc, dim.width, dim.height, backbuf);
 
 			EndPaint(window, &paint);
 		} break;
@@ -154,7 +155,7 @@ LRESULT CALLBACK win32_main_window_callback(
 
 int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int show_cmd) {
 	WNDCLASS window_class = {
-		.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
+		.style = CS_HREDRAW | CS_VREDRAW,
 		.lpfnWndProc = win32_main_window_callback,
 		.hInstance = instance,
 		// .hIcon = 
@@ -177,14 +178,17 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line
 			0
 		);
 
+		win32_resize_dbi_section(&backbuf, 1280, 720);
+
 		if (window) {
-			MSG message;
 			running = 1;
 
 			int x_offset = 0;
 			int y_offset = 0;
 
 			while (running) {
+				MSG message;
+
 				while (PeekMessage(&message, 0, 0, 0, PM_REMOVE)) {
 					if (message.message == WM_QUIT) {
 						running = false;
@@ -194,14 +198,11 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line
 					DispatchMessage(&message);
 				}
 
-				render_cool_gradient(x_offset, y_offset);
+				render_cool_gradient(backbuf, x_offset, y_offset);
 
 				HDC hdc = GetDC(window);
-				RECT client_rect;
-				GetClientRect(window, &client_rect);
-				int w = client_rect.right - client_rect.left;
-				int h = client_rect.bottom - client_rect.top;
-				win32_update_window(hdc, &client_rect, 0, 0, w, h);
+				win32_window_dimension dim = win32_get_window_dimension(window);
+				win32_display_buffer(hdc, dim.width, dim.height, backbuf);
 				ReleaseDC(window, hdc);
 
 				x_offset++;
